9e4d8c01f264d01b039da8112c0332f09f1a3359
emmalaechelt <emmalaechelt@naver.com>
2025-05-19 17:59:41 +0900

    Add files via upload

--- HRSTest.java
+++ HRSTest.java
@@ -1,4 +1,4 @@
-package edu.ruby.java.ch07.abstraction;
+package edu.ruby.java.ch08.polymophism;
 
 abstract class Employee {
 	String name;
@@ -9,28 +9,38 @@ abstract class Employee {
 }
 
 class Salesman extends Employee {
+	int annual_sales;
 	public void calcSalary() {
 		System.out.println("Salesman 급여 = 기본급 + 판매 수당");
 	}
-	
-	public void calcBonus( ) {
+
+	public void calcBonus() {
 		System.out.println("Salesman 보너스 = 기본급 * 12 * 4");
 	}
 }
-	
+
 class Consultant extends Employee {
+	int num_project;
 	public void calcSalary() {
 		System.out.println("Consultant 급여 = 기본급 + 컨설팅 특별 수당");
 	}
+
 	public void calcBonus() {
 		System.out.println("Consultant 보너스 = 기본급 * 12 * 2");
 	}
 }
 
-abstract class Manager extends Employee {
+class Manager extends Employee {
+	int num_team;
 	public void calcSalary() {
 		System.out.println("Manager 급여 = 기본급 + 팀 성과 수당");
 	}
+	@Override
+	public void calcBonus() {
+		// TODO Auto-generated method stub
+		System.out.println("Manager 보너스 = 기본급 * 12 * 6");
+	}
+}
 
 class Director extends Manager {
 	public void calcBonus() {
@@ -39,13 +49,31 @@ class Director extends Manager {
 }
 
 public class HRSTest {
+	public static void calcTax(Employee e) {
+		System.out.println("소득세를 계산합니다.");
+	}
+	
 	public static void main(String[] args) {
-		Salesman s = new Salesman();
-		Consultant c = new Consultant();
-		Director d = new Director();
-
-	s.calcBonus();
-	c.calcBonus();
-	d.calcBonus();
+		Salesman s1 = new Salesman();
+		Employee s2 = new Salesman();
+		Object s3 = new Salesman();
+		
+		Object m1 = new Manager();
+		Employee m2 = new Manager();
+		Manager m3 = new Manager();
+		
+		Object arr[] = new Object[6];
+		arr[0] = s1;
+		arr[1] = s2;
+		arr[2] = s3;
+		
+		arr[3] = m1;
+		arr[4] = m2;
+		arr[5] = m3;
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
 	}
 }
